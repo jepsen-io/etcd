a9be8e6fee67721b589c1170859d82077e12c5d6
Aphyr <aphyr@aphyr.com>
2019-12-30 18:48:26 -0500

    Jepsen 0.1.16

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.1.16-SNAPSHOT"]
+                 [jepsen "0.1.16"]
                  [tech.droit/clj-diff "1.0.1"]
                  ; jetcd has a bunch of dep ranges we have to pin
                  [io.etcd/jetcd-core "0.4.1"

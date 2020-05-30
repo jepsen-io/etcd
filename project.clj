d17c04bd2ae2623906d7ebccbc977523792a98d5
Aphyr <aphyr@aphyr.com>
2020-05-29 21:46:40 -0400

    0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen.etcd "0.1.0"
+(defproject jepsen.etcd "0.2.0"
   :description "etcd Jepsen test"
   :url "https://github.io/jepsen/etcd"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"

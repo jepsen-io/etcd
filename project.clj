40eac736bd56d477ab28d822986825a658cb30a8
Aphyr <aphyr@aphyr.com>
2022-10-02 17:39:03 -0400

    Jepsen 0.2.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [jepsen "0.2.7"]
+                 [jepsen "0.2.8-SNAPSHOT"]
                  [tech.droit/clj-diff "1.0.1"]
                  [io.etcd/jetcd-core "0.7.1"
                   ; jetcd pulls in so, SO many incompatible deps

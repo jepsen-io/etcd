ed89254acea8c3eaacd8054aa559ec1f6aab71c6
Aphyr <aphyr@aphyr.com>
2022-12-04 15:28:41 -0500

    jetcd 0.7.3

--- project.clj
+++ project.clj
@@ -4,15 +4,18 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [jepsen "0.2.8-SNAPSHOT"]
+                 [jepsen "0.2.8-SNAPSHOT"
+                  ; More jetcd conflicts
+                  :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [tech.droit/clj-diff "1.0.1"]
-                 [io.etcd/jetcd-core "0.7.1"
-                  ; jetcd pulls in so, SO many incompatible deps
+                 [io.etcd/jetcd-core "0.7.3"
+                  ; jetcd pulls in so, SO many incompatible deps with itself
                   :exclusions [io.netty/netty-codec-http2
+                               org.slf4j/slf4j-api
                                io.netty/netty-handler-proxy
-                               ]]
-                 [io.netty/netty-codec-http2 "4.1.74.Final"]
-                 [io.netty/netty-handler-proxy "4.1.74.Final"]
+                  ]]
+                 [io.netty/netty-codec-http2 "4.1.78.Final"]
+                 [io.netty/netty-handler-proxy "4.1.78.Final"]
                  [cheshire "5.11.0"]
                  ]
   :jvm-opts ["-Djava.awt.headless=true"

4d7213261bd7dc5f74eafe8e8128a33a2ea42ef6
Kyle Kingsbury <aphyr@aphyr.com>
2024-03-03 20:19:42 -0600

    Dep bumps

--- project.clj
+++ project.clj
@@ -4,19 +4,19 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [jepsen "0.3.3-SNAPSHOT"
+                 [jepsen "0.3.5"
                   ; More jetcd conflicts
                   :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [tech.droit/clj-diff "1.0.1"]
-                 [io.etcd/jetcd-core "0.7.3"
+                 [io.etcd/jetcd-core "0.7.7"
                   ; jetcd pulls in so, SO many incompatible deps with itself
                   :exclusions [io.netty/netty-codec-http2
                                org.slf4j/slf4j-api
                                io.netty/netty-handler-proxy
                   ]]
-                 [io.netty/netty-codec-http2 "4.1.78.Final"]
-                 [io.netty/netty-handler-proxy "4.1.78.Final"]
-                 [cheshire "5.11.0"]
+                 [io.netty/netty-codec-http2 "5.0.0.Alpha2"]
+                 [io.netty/netty-handler-proxy "5.0.0.Alpha2"]
+                 [cheshire "5.12.0"]
                  ]
   :jvm-opts ["-Djava.awt.headless=true"
              "-server"

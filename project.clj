f235df892618e814cb42de39fe9b4c6f30d66a01
Kyle Kingsbury <aphyr@aphyr.com>
2025-01-09 07:15:51 -0600

    Bump to Jepsen 0.3.7

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject jepsen.etcd "0.2.4"
+(defproject jepsen.etcd "0.2.5-SNAPSHOT"
   :description "etcd Jepsen test"
   :url "https://github.io/jepsen/etcd"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.3"]
-                 [jepsen "0.3.5"
+                 [jepsen "0.3.7"
                   ; More jetcd conflicts
                   :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [tech.droit/clj-diff "1.0.1"]
